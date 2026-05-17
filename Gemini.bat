39a1475ba0dbe12ddc04e89aa8bdde6e32d385de
yuta <y.takak1234@gmail.com>
2026-05-17 09:50:34 +0900

    changed

--- Gemini.bat
+++ Gemini.bat
@@ -1,6 +1,6 @@
 @echo off
 title Gemini - 起動しています...
-python --version>nul
+python --version
 if not %errorlevel% == 0 (
     title Gemini - Pythonがインストールされていません
     curl  -L -O "https://www.python.org/ftp/python/3.14.4/python-3.14.4-amd64.exe"
@@ -9,11 +9,9 @@ if not %errorlevel% == 0 (
 )
 
 if not exist venv (
-    title Gemini - 環境を作成しています...
     python -m venv venv
 )
 call venv\Scripts\activate.bat
-title Gemini - パッケージをインストールしています...
 pip install gradio google-genai
 title Gemini - ようこそ
 python gemini.py
--- gemini.py
+++ gemini.py
@@ -6,7 +6,7 @@ from google import genai
 
 if os.path.exists('.txt'):
     root = tk.Tk()
-    root.title("Gemini")
+    root.title("Gemini - APIキーの読み込みに成功しました")
     root.state("zoomed")
     label1 = tk.Label(root, text="Gemini", font=("Arial", 30))
     label1.pack()
@@ -19,7 +19,7 @@ if os.path.exists('.txt'):
     root.mainloop()
 else:
     root = tk.Tk()
-    root.title("Gemini")
+    root.title("Gemini - APIキーの入力")
     root.state("zoomed")
     def link_click(url):
         webbrowser.open_new(url)
@@ -28,7 +28,7 @@ else:
     img = tk.PhotoImage(file="logo.png")
     label2 = tk.Label(root, image=img, font=("Arial", 30))
     label2.pack()
-    label3 = tk.Label(root, text="APIキーを取得して入力してください", fg="blue", cursor="hand2", font=("Arial", 10), underline=True)
+    label3 = tk.Label(root, text="APIキーをここから取得して入力してください", fg="blue", cursor="hand2", font=("Arial", 10), underline=True)
     label3.pack(pady=(0, 10))
     label3.bind("<Button-1>",lambda e:link_click("https://aistudio.google.com/api-keys"))
     entry = tk.Entry(root, show="*", width=30)
